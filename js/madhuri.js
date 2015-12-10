34d14ea30cdfcf0612bd60026114a84c3b31efa6
Madhuri Palle <madhuri.palle@yahoo.com>
2015-12-10 01:32:15 -0500

    Tab choice extraction
    Added divs for search results, extracted tab choices using jQuery,
    fixed one error, changed css.

--- js/madhuri.js
+++ js/madhuri.js
@@ -1,4 +1,37 @@
-var currentbrowsetab;
+var currentbrowsetab = "Browse Playlists";
+var subtabplaylist = "Browse";
+var subtabalbum = "Browse";
+var subtabtrack = "Browse";
+
+$('.playlists a').on('shown.bs.tab', function(event){
+	subtabplaylist = $(event.target).text(); 
+});
+
+$('.albums a').on('shown.bs.tab', function(event){
+	subtabalbum = $(event.target).text(); 
+});	
+
+$('.tracks a').on('shown.bs.tab', function(event){
+	subtabtrack = $(event.target).text(); 
+});	
+
+$('.nav-stacked a').on('shown.bs.tab', function(event){
+	currentbrowsetab = $(event.target).text(); 
+});
+
+
+function CheckTabActivity() {
+	console.log(currentbrowsetab);
+	if(currentbrowsetab=="Browse Playlists")
+		console.log(subtabplaylist);
+	else if(currentbrowsetab=="Browse Albums")
+		console.log(subtabalbum);
+	else if(currentbrowsetab=="Browse Tracks")
+		console.log(subtabtrack);
+	else
+		console.log("Error choosing tabs");
+}
+
 
 function unhideme (divid) {
 	var item = document.getElementById(divid);
@@ -32,3 +65,4 @@ function RemoveCarosuel() {
 	unhideme("searchid");
 	unhideme("footerid");
 }
+
