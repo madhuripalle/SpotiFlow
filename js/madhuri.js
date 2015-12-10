9105ba85f20888f2e441ced3e7cdac71a9b9509d
Madhuri Palle <madhuri.palle@yahoo.com>
2015-12-10 02:20:26 -0500

    Text input for search
    Got text inputs for search, changed css to make it look cleaner, added
    js functionality, fixed bug

--- js/madhuri.js
+++ js/madhuri.js
@@ -2,6 +2,9 @@ var currentbrowsetab = "Browse Playlists";
 var subtabplaylist = "Browse";
 var subtabalbum = "Browse";
 var subtabtrack = "Browse";
+var playlist_input;
+var album_input;
+var track_input;
 
 $('.playlists a').on('shown.bs.tab', function(event){
 	subtabplaylist = $(event.target).text(); 
@@ -23,13 +26,78 @@ $('.nav-stacked a').on('shown.bs.tab', function(event){
 function CheckTabActivity() {
 	console.log(currentbrowsetab);
 	if(currentbrowsetab=="Browse Playlists")
+	{
+		playlist_input = document.getElementById("playlist_text").value;
 		console.log(subtabplaylist);
+		if(subtabplaylist=="Featured")
+		{
+			//call API for featured playlists
+			console.log("Featured playlist list will be shown");
+		}
+		else if(subtabplaylist=="Browse")
+		{
+			//call API for public playlist content
+			console.log("Browsing for " + playlist_input);
+		}
+		else if(subtabplaylist=="My Playlists")
+		{
+			console.log("Personal Content for " + playlist_input);
+		}
+		else
+		{
+			console.log("error in playlist call");
+		}
+	}
+		
 	else if(currentbrowsetab=="Browse Albums")
+		{
+		album_input = document.getElementById("album_text").value;
 		console.log(subtabalbum);
+		if(subtabalbum=="Featured")
+		{
+			//call API for featured playlists
+			console.log("Featured albums list will be shown");
+		}
+		else if(subtabalbum=="Browse")
+		{
+			//call API for public playlist content
+			console.log("Browsing for " + album_input);
+		}
+		else if(subtabalbum=="My Albums")
+		{
+			console.log("Personal Content for " + album_input);
+		}
+		else
+		{
+			console.log("error in album call");
+		}
+	}
 	else if(currentbrowsetab=="Browse Tracks")
-		console.log(subtabtrack);
+		{
+		track_input = document.getElementById("track_text").value;
+		console.log(subtabalbum);
+		if(subtabtrack=="Featured")
+		{
+			//call API for featured playlists
+			console.log("Featured tracks list will be shown");
+		}
+		else if(subtabtrack=="Browse")
+		{
+			//call API for public playlist content
+			console.log("Browsing for " + track_input);
+		}
+		else if(subtabtrack=="My Tracks")
+		{
+			console.log("Personal Content for " + track_input);
+		}
+		else
+		{
+			console.log("error in track call");
+		}
+	}
 	else
 		console.log("Error choosing tabs");
+
 }
 
 
