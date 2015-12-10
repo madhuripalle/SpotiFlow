5919b040f6efaba9ce9e0bfe94109b3af5e4da36
clsd3294 <clsd_mmh_3294@hotmail.com>
2015-12-10 02:56:47 -0500

    Selection Page, More choose interactions

--- js/selection-page.js
+++ js/selection-page.js
@@ -3,7 +3,7 @@ $("#durationinput").prop('disabled',true);
 $('[data-toggle="tooltip"]').tooltip();
 // $("#prioritysectionid").prop('disabled',false);
 
-//set a threshold to test the funtion
+//set a threshold to test the function
 var duration_thresh = 600;
 
 $("#durationbtn").on('switchChange.bootstrapSwitch',function(event,state){
@@ -48,10 +48,97 @@ $('#nextpage').on('click',function(e){
 			}
 		}
 	}
+	else{
+		//No requirement for the duration. 
+	}
 
-})
+});
 
-function computeDuration (){
+$('#bactrianflow').on('click',function(e){
+	e.preventDefault();
+
+	$('#cocktailflow').prop('disabled',true);
+	$('.drome').prop('disabled',true);
+	$('.descend').prop('disabled',true);
+	$('.ascend').prop('disabled',true);
+	$('#chooseagain').show();
+
+});
+
+$('#cocktailflow').on('click',function(e){
+	e.preventDefault();
+
+	$('#bactrianflow').prop('disabled',true);
+	$('.drome').prop('disabled',true);
+	$('.descend').prop('disabled',true);
+	$('.ascend').prop('disabled',true);
+	$('#chooseagain').show();
+});
+
+$('#dromedaryflow').on('click',function(e){
+	e.preventDefault();
+
+	$('.descend').prop('disabled',true);
+	$('.ascend').prop('disabled',true);
+	$('.flowbtn1').prop('disabled',true);
+	$('#chooseagain').show();
+});
+
+$('#descendflow').on('click',function(e){
+	e.preventDefault();
+
+	$('.drome').prop('disabled',true);
+	$('.ascend').prop('disabled',true);
+	$('.flowbtn1').prop('disabled',true);
+	$('#chooseagain').show();
+});
+
+$('#ascendflow').on('click',function(e){
+	e.preventDefault();
+
+	$('.descend').prop('disabled',true);
+	$('.drome').prop('disabled',true);
+	$('.flowbtn1').prop('disabled',true);
+	$('#chooseagain').show();
+});
+
+$('.flowbtn2').on('click',function(e){
+	e.preventDefault();
+	$('.attr').prop('disabled',false);
+	$('#prioritysectionid h4').removeClass('text-muted');
+	$('#prioritysectionid h4').addClass('text-default');
+	// $('.priorder').prop('disabled',false);
+});
+
+//A way we can priorize the order of the three attributes.
+// $('.priorder').on('click',function(e){
+// 	e.preventDefault();
+
+// 	if($('#priorityresult').children().length >=3){
+// 		alert("You can only choose at most three attributes!");
+// 		$('#attrpriorityid input[type=checkbox]:checked').prop('checked',false);
+// 		return;
+// 	}
+// 	if(!$('.priorder').prop('disabled')){
+// 		$('#prioritysectionid input[type=checkbox]').each(function(){
+// 			if($(this).prop('checked')){
+// 				console.log(2);
+// 				console.log($(this).val());
+
+// 				var item = "<div><li>" + 
+// 					$(this).val() +
+// 					"</li></div>";
+// 				console.log(item);
+// 				$('#priorityresult').append(item);
+// 				$(this).prop('checked',false);
+// 			}
+// 		});
+// 	}
+// })
+
+
+
+function computeDuration(){
 	var hour = $('#hourid option:selected').val();
 	var min = $('#minuteid option:selected').val();
 	var second = $('#secondid option:selected').val();
@@ -61,6 +148,28 @@ function computeDuration (){
 
 
 }
+
+function enableAll() {
+	// if(!$('#drome').prop('disabled')){
+	// 	console.log(1);
+	// }
+	
+
+	$('.flowbtn1').prop('disabled',false);
+	$('.drome').prop('disabled',false);
+	$('.descend').prop('disabled',false);
+	$('.ascend').prop('disabled',false);
+	$('.attr').prop('disabled',true);
+	$('.attr').prop('checked',false);
+	if(!$('.drome').prop('disabled')||!$('.descend').prop('disabled')||!$('.ascend').prop('disabled')){
+		$("#prioritysectionid h4").removeClass('text-default');
+		$("#prioritysectionid h4").addClass('text-muted');
+		// $('.priorder').prop('disabled',true);
+	}
+	$('#chooseagain').hide();
+}
+
+
 
 
 
