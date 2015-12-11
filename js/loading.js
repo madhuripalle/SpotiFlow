7372defc0f9a78ff3de94bce43b8b718ef1dbff4
Morgan Thompson <mzt2102@columbia.edu>
2015-12-11 18:11:21 -0500

    Added loading spinners to search results (test mode) and fixed layout of search results

--- js/loading.js
+++ js/loading.js
@@ -1,37 +1,53 @@
-    /*
-     *  Show loading div with spinner (spin.min.js)
-     */
-    var loadSpinners = function () {
-        $('.loading').show();
-        var opts = {
-            lines: 7 // The number of lines to draw
-            , length: 2 // The length of each line
-            , width: 2 // The line thickness
-            , radius: 5 // The radius of the inner circle
-            , scale: 1 // Scales overall size of the spinner
-            , corners: 1 // Corner roundness (0..1)
-            , color: '#000' // #rgb or #rrggbb or array of colors
-            , opacity: 0.25 // Opacity of the lines
-            , rotate: 0 // The rotation offset
-            , direction: 1 // 1: clockwise, -1: counterclockwise
-            , speed: 0.7 // Rounds per second
-            , trail: 42 // Afterglow percentage
-            , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-            , zIndex: 2//2e9 // The z-index (defaults to 2000000000)
-            , className: 'spinner' // The CSS class to assign to the spinner
-            , top: '50%' // Top position relative to parent
-            , left: '50%' // Left position relative to parent
-            , shadow: false // Whether to render a shadow
-            , hwaccel: false // Whether to use hardware acceleration
-            , position: 'absolute' // Element positioning
-        };
-        var targets = document.getElementsByClass('loading');
-        var spinners = [];
-        for (var i in targets) {
-            spinners.add(new Spinner(opts).spin(targets[i]));
-        }
-        //var spinner = new Spinner(opts).spin(target);
-        // Add individual iframe.load( hide the child .loading element )
-    });
+/*
+ *  Show loading div with spinner (spin.min.js)
+ */
+function LoadSpinners () {
+    console.log("loadSpinners called");
+    $('.loading').show();
+    var opts = {
+        lines: 7 // The number of lines to draw
+        , length: 2 // The length of each line
+        , width: 2 // The line thickness
+        , radius: 5 // The radius of the inner circle
+        , scale: 1 // Scales overall size of the spinner
+        , corners: 1 // Corner roundness (0..1)
+        , color: '#000' // #rgb or #rrggbb or array of colors
+        , opacity: 0.25 // Opacity of the lines
+        , rotate: 0 // The rotation offset
+        , direction: 1 // 1: clockwise, -1: counterclockwise
+        , speed: 0.7 // Rounds per second
+        , trail: 42 // Afterglow percentage
+        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+        , zIndex: 2e9//2e9 // The z-index (defaults to 2000000000)
+        , className: 'spinner' // The CSS class to assign to the spinner
+        , top: '50%' // Top position relative to parent
+        , left: '50%' // Left position relative to parent
+        , shadow: false // Whether to render a shadow
+        , hwaccel: false // Whether to use hardware acceleration
+        , position: 'absolute' // Element positioning
+    };
+    var targets = document.getElementsByClassName('loading');
+    var spinners = [];
+    for (var i in targets) {
+        spinners.push(new Spinner(opts).spin(targets[i])); //throwing an error
 
-    // on result iframe load, remove spinner
+        // this may fail if we receive less than ten results
+        // or if doesn't gether targets in order
+        var idNum = i+1;
+        $('#i'+idNum).load(function () {
+            spinners[i].stop();
+            $('#i'+idNum).find('.loading').hide();
+        });
+    }
+    // breaks it currenty.
+    /*
+    $('.pager')click(function () {
+        for (var i in spinners){
+            var idNum = i+1;
+            spinners[i].stop();
+            $('#i'+idNum).find('.loading').hide();
+        }
+    });*/
+};
+
+// on result iframe load, remove spinner
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,13 +1,16 @@
-//var setIFrameHeight = function () {
-function setIFrameWidth () {
+function SetIFrameWidth () {
+    console.log("setIFrameWidth called");
     var width = Math.min($('#sidebar').width(), 640);
     $('#current-pl').attr('width', width);
 };
 
-$('#carouselbtn').click(setIFrameWidth);
-
+$('#carouselbtn').click(function(e) {
+    SetIFrameWidth();
+    // To test
+    LoadSpinners();
+});
 
 // Reset iframe height after window resize
-$(window).resize(setIFrameWidth);  
+$(window).resize(SetIFrameWidth);  
 
 // Need a var for the created playlist (object and/or URI)
