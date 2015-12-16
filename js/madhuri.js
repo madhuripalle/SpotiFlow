376db661becc36b55293890c208395c736fd46a6
clsd3294 <clsd_mmh_3294@hotmail.com>
2015-12-16 16:25:06 -0500

    Open playlist button, need userid and playlistid

--- js/madhuri.js
+++ js/madhuri.js
@@ -69,6 +69,12 @@ $("#nextstep").click(function(){
 	SetIFrameSize();
 });
 
+$("#openPlaylist").on('click',function(){
+	// var link = "https://play.spotify.com/user/spotify/playlist/59ZbFPES4DQwEjBpWHzrtC";
+	var link = "https://play.spotify.com/" + userid + "/" + "spotify/playlist/" + currentPlaylistId;
+	window.open(link, '_blank');
+});
+
 function processAdd(elementid)
 {
 	
