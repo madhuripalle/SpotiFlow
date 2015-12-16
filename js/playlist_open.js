7d6a1835a291ecc7a134f950884075628235b536
clsd3294 <clsd_mmh_3294@hotmail.com>
2015-12-16 13:12:08 -0500

    Open current playlist button

---
+++ js/playlist_open.js
@@ -0,0 +1,8 @@
+var userid;
+var currentPlaylistId;
+
+$("#openPlaylist").on('click',function(){
+	var link = "https://play.spotify.com/user/spotify/playlist/59ZbFPES4DQwEjBpWHzrtC";
+	// var link = "https://play.spotify.com/" + userid + "/" + "spotify/playlist/" + currentPlaylistId;
+	window.open(link, '_blank');
+});
