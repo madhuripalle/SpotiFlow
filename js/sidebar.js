aa5cdfb98f7c6754e637ffbded29d5be0fdf635d
Morgan Thompson <mzt2102@columbia.edu>
2015-12-16 11:24:47 -0500

    Made current playlist fixed sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,16 +1,32 @@
-function SetIFrameWidth () {
-    console.log("setIFrameWidth called");
+function SetIFrameSize () {
+    console.log("setIFrameSize called");
+    // Set sidebar and current-pl width
+    var containerRightMargin = ($('div.container:last').outerWidth(true)-$('div.container:last').width()) / 3;
+    $('#sidebar').css('margin-right', containerRightMargin+'px');
+    $('#sidebar').css('width', '33.3333333%');
+    var sidebarWidth = $('#sidebar').width()-containerRightMargin;
+    $('#sidebar').css('width', sidebarWidth+'px');
     var width = Math.min($('#sidebar').width(), 640);
     $('#current-pl').attr('width', width);
+
+    // Set sidebar and current-pl height
+    var activePage = $('.background-choice').filter(function() {
+        return $(this).css('display') === 'block';
+    });
+    var sidebarHeight = activePage.height() - $('#prevstep').parent().outerHeight(true);
+    $('#sidebar').css('height', sidebarHeight);
+    var height = Math.min($('#sidebar').height()-$('#sidebar').find('h3').outerHeight(true)-$('#sidebar').find('p').outerHeight(true), 720);
+    $('#current-pl').attr('height', height);
 };
 
 $('#carouselbtn').click(function(e) {
-    SetIFrameWidth();
+    SetIFrameSize();
     // To test
-    LoadSpinners(4);
+    //LoadSpinners(4);
 });
 
 // Reset iframe height after window resize
-$(window).resize(SetIFrameWidth);  
+$(window).resize(SetIFrameSize);
+
 
 // Need a var for the created playlist (object and/or URI)
