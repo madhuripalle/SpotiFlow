1fd9ba5d333b2eac93fb7cd83b46ccd1deba75b1
clsd3294 <clsd_mmh_3294@hotmail.com>
2015-12-16 13:14:09 -0500

    Edit the button

--- js/playlist_open.js
+++ js/playlist_open.js
@@ -2,7 +2,7 @@ var userid;
 var currentPlaylistId;
 
 $("#openPlaylist").on('click',function(){
-	var link = "https://play.spotify.com/user/spotify/playlist/59ZbFPES4DQwEjBpWHzrtC";
-	// var link = "https://play.spotify.com/" + userid + "/" + "spotify/playlist/" + currentPlaylistId;
+	// var link = "https://play.spotify.com/user/spotify/playlist/59ZbFPES4DQwEjBpWHzrtC";
+	var link = "https://play.spotify.com/" + userid + "/" + "spotify/playlist/" + currentPlaylistId;
 	window.open(link, '_blank');
 });
